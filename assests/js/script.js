f64033dc69670e60e3e8a5fca115c64dd1e8ab71
Andrew Gettz <drews_computer@Andrews-MacBook-Pro.local>
2022-06-15 21:50:00 -0400

    updated comments per requirements

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,13 +1,17 @@
 
 
-
+//link to button 
 const generateBtn = document.querySelector ("#generate");
 
+//link to password 
 const password = document.querySelector("#password")
 
+//define function (20) sets length to 20 characters 
 function writePassword(length  = 20){
+//Set characters for the random password
 const randomPassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$!%^&*()_+=-";
 
+//generates and returns password randomly as stored variable passGen
 let passGen = ""; 
 for(let i = 0; i < length; ++i){
   let at = Math.floor(Math.random() * (randomPassword.length + 1)); 
@@ -16,6 +20,7 @@ for(let i = 0; i < length; ++i){
 return passGen; 
 }; 
 
+//Listens for click button and runs writePassword function 
 generateBtn.addEventListener("click",() => {
   password.value = [writePassword(20)];
 }); 
